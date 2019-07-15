82ce93094466ee929f69d5836cff0f9c4f9135c9
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-07-15 23:44:53 +0200

    Added feature to add people to the list, made possible to remove them and clear whole list. Also added form validation

--- main.js
+++ main.js
@@ -29,3 +29,57 @@ function smoothScroll(target, duration){
 document.querySelector('.header__btn').addEventListener('click', function(){
     smoothScroll('main', 1000);
 });
+
+// People list
+let people = [];
+
+
+let btnAddPerson = document.querySelector('.people__btn');
+let inputPerson = document.querySelector('.people__input');
+
+// Clear people list
+document.querySelector('.people__clearBtn').addEventListener('click', function(){
+    document.querySelector('.people__list').innerHTML = '';
+})
+// Validate new person
+btnAddPerson.addEventListener('click', function(el){
+    el.preventDefault();
+    if(inputPerson.value === '' || inputPerson.value.length >= 18){
+        return alert('Check if field is empty or if it has more than 17 characters');
+    }
+    else if(people.includes(inputPerson.value)){
+        return alert('You have already added this person');
+    }
+    people.unshift(inputPerson.value);
+    inputPerson.value = '';
+    updatePeopleList();
+})
+
+function updatePeopleList() {
+    // Insert each person on the website
+    document.querySelector('.people__list').innerHTML = people.map(person => {
+        return (`
+            <p class="people__person" fullName='${person}'>
+                ${person}
+                <img class='people__deleteBtn' src="./gallery/deleteBtn.svg" alt="deleteBtn">
+            </p>
+        `)
+    }).join('');
+
+    // Update event listener
+    updateBtnDelPerson();
+}
+
+function updateBtnDelPerson(){
+    let btnDelPerson = document.querySelectorAll('.people__deleteBtn');
+    btnDelPerson.forEach(btn => {
+        btn.addEventListener('click', delPerson);
+    })
+}
+
+function delPerson(){
+    people = people.filter((person) => {
+        return person !== this.parentNode.getAttribute('fullName');
+    });
+    this.parentNode.style.display = 'none';
+}
