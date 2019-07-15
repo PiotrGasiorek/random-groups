8bd9e3c02b54436803ff6e12fee1f13b78ca4ebf
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-07-15 18:38:50 +0200

    Added smooth scroll

---
+++ main.js
@@ -0,0 +1,31 @@
+// Smooth scroll
+function smoothScroll(target, duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition + 1;
+    var startTime = null;
+    
+    function animation(currentTime){
+        console.log(currentTime);
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if(t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+	
+    requestAnimationFrame(animation);
+}
+
+// Call to action smooth scroll
+document.querySelector('.header__btn').addEventListener('click', function(){
+    smoothScroll('main', 1000);
+});
