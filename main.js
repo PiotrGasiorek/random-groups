a852bfd9592eceb08f3b43fe94204476fd23eedb
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-07-16 13:35:21 +0200

    Added functionality for range slider

--- main.js
+++ main.js
@@ -83,3 +83,19 @@ function delPerson(){
     });
     this.parentNode.style.display = 'none';
 }
+
+// Range slider functionality 
+let slider = document.querySelector('#slider');
+let sliderOutput = document.querySelector('.groups__label--output');
+
+sliderOutput.innerHTML = slider.value;
+
+slider.oninput = function () {
+    sliderOutput.innerHTML = Math.floor(this.value / 10);
+}
+
+slider.addEventListener('mousemove', function(){
+    var x = slider.value;
+    var color = 'linear-gradient(90deg, rgb(23, 107, 239)' + x + '%, rgb(124, 124, 124)' + x + '%)';
+    slider.style.background = color;
+})
