312f2b6a5caa5e5050c8afe1377e89481cfd5119
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-10-22 11:03:02 +0200

    Organized code. Implemented DRY

--- js/popups.js
+++ js/popups.js
@@ -1,21 +1,25 @@
-let wrapper = document.querySelector('.wrapper');
-
-function displayPopup(message){
+const wrapper = document.querySelector('.wrapper');
+function popupToggle(toggle){
+  if(toggle == 'on'){
+    // Activate popup
     wrapper.style.display = 'block';
-    document.querySelector('.popup__message').innerHTML = message;
-
-    document.querySelector('.popup__btn').onclick = function() {
-        wrapper.style.display = 'none';
-    }
-
-    window.onclick = function(event) {
-        if (event.target == wrapper) {
-          wrapper.style.display = "none";
-        }
-      }
+    // Disactivate input
+    document.querySelector('.people__input').blur();
+  }
+  else{
+    // Disactivate popup
+    wrapper.style.display = 'none';
+    // Activate input
+    document.querySelector('.people__input').focus();
+  }
 }
-
-
-
+function displayPopup(message){
+    popupToggle('on');
+    document.querySelector('.popup__message').innerHTML = message;
+    // Turn popup off when...
+    document.querySelector('.popup__btn').onclick = () => popupToggle('off');
+    document.onkeydown = () => popupToggle('off');
+    window.onclick = (event) => event.target == wrapper ? popupToggle('off') : false;
+}
 
 export { displayPopup };
